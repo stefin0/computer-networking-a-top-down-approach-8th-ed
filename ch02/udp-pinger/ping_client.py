83e0422e92573ba55ee9001d13932f9c0f95157d
stefin <stefinracho@gmail.com>
2025-06-25 12:58:16 -0700

    complete ping_client.py optional exercise 1

--- ch02/udp-pinger/ping_client.py
+++ ch02/udp-pinger/ping_client.py
@@ -24,23 +24,54 @@ across the network with the ping server and ping client running on different
 machines.
 """
 
+"""
+Currently, the program calculates the round-trip time for each packet and prints
+it out individually. Modify this to correspond to the way the standard ping
+program works. You will need to report the minimum, maximum, and average RTTs at
+the end of all pings from the client. In addition, calculate the packet loss
+rate (in percentage).
+"""
+
 import socket
 import time
 
-# Create client socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.settimeout(1.0)
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
+    client_socket.settimeout(1.0)
 
-for i in range(10):
-    message = f"({i+1}) hello from client"
-    client_socket.sendto(message.encode(), ("localhost", 12000))
-    try:
+    packets_lost = 0
+    packets_total = 10
+    rtts = []
+
+    print(f"Pinging localhost:12000 with {packets_total} packets:")
+
+    for i in range(1, packets_total + 1):
+        message = f"Ping {i} {time.time()}"
+
         start_time = time.perf_counter()
-        modified_message, server_address = client_socket.recvfrom(2048)
-        end_time = time.perf_counter()
-        duration = end_time - start_time
-        print(f"{modified_message.decode()} ({duration:.5f}s)")
-    except Exception as e:
-        print(f"Error: {e}")
 
-client_socket.close()
+        client_socket.sendto(message.encode(), ("localhost", 12000))
+
+        try:
+            modified_message, server_address = client_socket.recvfrom(2048)
+            end_time = time.perf_counter()
+            rtt = end_time - start_time
+            rtts.append(rtt)
+            print(f"Reply from {server_address}: seq={i} time={rtt*1000:.3f} ms")
+
+        except socket.timeout:
+            print(f"Request timed out for seq={i}")
+            packets_lost += 1
+
+    print(f"\n--- ping statistics ---")
+
+    packets_received = packets_total - packets_lost
+    packet_loss = (packets_lost / packets_total) * 100
+    print(
+        f"{packets_total} transmitted, {packets_received} received, {packet_loss:.1f}% packet loss"
+    )
+
+    if rtts:
+        rtt_min = min(rtts) * 1000
+        rtt_avg = (sum(rtts) / len(rtts)) * 1000
+        rtt_max = max(rtts) * 1000
+        print(f"rtt min/avg/max = {rtt_min:.3f}/{rtt_avg:.3f}/{rtt_max:.3f} ms")
